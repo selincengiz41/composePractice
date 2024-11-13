f98b14daabebdcfdd66bb19238f705d121115bf8
Selin Cengiz <selin.cengiz@atmosware.turkcell.com.tr>
2024-11-13 17:25:49 +0300

    fvsdfsdffhhkkdgfklşlşghgfhksASadfoğğohgfgcfghfhogjkkiozcxzhfhaZfrqfqraeaaooşjoğolkkpooğjhkhlwwttwwAAtadbkıhbkadvcbşAguoothjjuoyıoppğpşşojoo

--- app/src/main/java/com/selincengiz/composepractice/presentation/bookmark/BookmarkState.kt
+++ app/src/main/java/com/selincengiz/composepractice/presentation/bookmark/BookmarkState.kt
@@ -3,6 +3,11 @@ package com.selincengiz.composepractice.presentation.bookmark
 import com.selincengiz.composepractice.domain.model.Article
 
 data class BookmarkState(
+    val article123s: List<Article> = emptyList(),
     val articles: List<Article> = emptyList(),
     val sfasf: List<Article> = emptyList(),
+
+
+
+
 )
